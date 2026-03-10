f35218a7ff426e91002852bb18749ff4dce3b5d8
Ильнар <persone716@gmail.com>
2026-03-10 09:07:01 +0300

    Добавить вопросы\ответы\комментв плюс тесты

--- tests/FakerRuTest.php
+++ tests/FakerRuTest.php
@@ -5,6 +5,9 @@ use PHPUnit\Framework\TestCase;
 
 class FakerRuTest extends TestCase
 {
+    /**
+     * relations expertShortDescription
+     */
     public function testExpertShortDescriptionOnRelations()
     {
         $testObject = new FakerRu();
@@ -14,6 +17,9 @@ class FakerRuTest extends TestCase
         $this->assertNotEmpty($result);
     }
 
+    /**
+     * veterinary expertShortDescription
+     */
     public function testExpertShortDescriptionOnVeterinary()
     {
         $testObject = new FakerRu();
@@ -23,6 +29,9 @@ class FakerRuTest extends TestCase
         $this->assertNotEmpty($result);
     }
 
+    /**
+     * relations expertDescription
+     */
     public function testExpertDescriptionOnRelations()
     {
         $testObject = new FakerRu();
@@ -32,14 +41,111 @@ class FakerRuTest extends TestCase
         $this->assertNotEmpty($result);
     }
 
+    /**
+     * veterinary expertDescription
+     */
     public function testExpertDescriptionOnVeterinary()
     {
         $testObject = new FakerRu();
         $result = $testObject->expertDescription(
             'nevrologiya', 'koshki', 'veterinary'
         );
-        var_dump($result);
-        exit;
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * relations titleQuestion
+     */
+    public function testTitleQuestionOnRelations()
+    {
+        $testObject = new FakerRu();
+        $result = $testObject->titleQuestion(
+            null, 'relations'
+        );
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * veterinary titleQuestion
+     */
+    public function testTitleQuestionOnVeterinary()
+    {
+        $testObject = new FakerRu();
+        $result = $testObject->titleQuestion(
+            null, 'veterinary'
+        );
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * relations descriptionQuestion
+     */
+    public function testDescriptionQuestionOnRelations()
+    {
+        $testObject = new FakerRu();
+        $result = $testObject->descriptionQuestion(
+            null, 'relations'
+        );
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * veterinary descriptionQuestion
+     */
+    public function testDescriptionQuestionOnVeterinary()
+    {
+        $testObject = new FakerRu();
+        $result = $testObject->descriptionQuestion(
+            null, 'veterinary'
+        );
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * relations answerQuestion
+     */
+    public function testAnswerQuestionOnRelations()
+    {
+        $testObject = new FakerRu();
+        $result = $testObject->answerQuestion(
+            null, 'relations'
+        );
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * veterinary answerQuestion 
+     */
+    public function testAnswerQuestionOnVeterinary()
+    {
+        $testObject = new FakerRu();
+        $result = $testObject->answerQuestion(
+            null, 'veterinary'
+        );
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * relations commentQuestion
+     */
+    public function testCommentQuestionOnRelations()
+    {
+        $testObject = new FakerRu();
+        $result = $testObject->commentQuestion(
+            null, 'relations'
+        );
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * veterinary commentQuestion
+     */
+    public function testCommentQuestionOnVeterinary()
+    {
+        $testObject = new FakerRu();
+        $result = $testObject->commentQuestion(
+            null, 'veterinary'
+        );
         $this->assertNotEmpty($result);
     }
 }
